13e8b3f45b305fb1cac006509127326e3b61267f
Doug Kintop <dkintop@hotmail.com>
2019-12-19 12:14:38 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -63,3 +63,23 @@ function addToyForm() {
       .then(console.log);
   });
 }
+
+function likes(e) {
+  e.preventDefault();
+  let more = parseInt(e.target.previousElementSibling.innerText) + 1;
+
+  fetch(`http://localhost:3000/toys/${e.target.id}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      likes: more
+    })
+  })
+    .then(res => res.json())
+    .then(like_obj => {
+      e.target.previousElementSibling.innerText = `${more} likes`;
+    });
+}
