64939a0d008a02e0635df4b6e7717c51ffeb7b14
Rsja12 <asocarras10@gmail.com>
2019-12-20 20:19:55 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -15,4 +15,62 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
 })
 
-// test
+// get toys
+function getToys() {
+  fetch("http://localhost:3000/toys")
+  .then(resp => resp.json())
+  .then(toys => {
+    toys.forEach(toy => displayToy(toy))
+  })
+}
+
+// HTML toy card
+function toyCard(toy) {
+  return `
+    <div class="card">
+      <h2>${toy.name}</h2>
+      <img src=${toy.image} class="toy-avatar" />
+      <p>${toy.likes} Likes </p>
+      <button class="like-btn">Like <3</button>
+    </div>
+  `
+}
+
+// append to HTML
+const displayToy = (toy) => {
+  const toyCollection = document.getElementById("toy-collection")
+  toyCollection.innerHTML += toyCard(toy)
+}
+
+// function addToyForm() {
+//   const newToyForm = document.querySelector(".add-toy-form")
+//   newToyForm.addEventListener("submit", function() {
+//     debugger
+//   })
+// }
+
+
+// Not working......  .addEventListener
+function addToyForm(){
+  const newToyForm = document.querySelector(".add-toy-form")
+  newToyForm.addEventListener("submit", function(event){
+    event.preventDefault()
+    let nameInput = this.querySelector("#toy-name").value
+    let imageInput = this.querySelector("#toy-image").value
+    fetch("http://localhost:3000/toys/", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        name: `${nameInput}`,
+        image: `${imageInput}`,
+        likes: 0
+      })
+    }).then(resp => resp.json())
+    .then(displayToy)
+  })
+}
+
+getToys()
+addToyForm()
