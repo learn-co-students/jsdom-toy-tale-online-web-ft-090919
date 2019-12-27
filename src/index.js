2efff8f80a93cc0307b3d393ec826a09321f4084
ewenaiwe <ewenaiwe@gmail.com>
2019-12-27 19:00:48 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -14,3 +14,36 @@ document.addEventListener("DOMContentLoaded", ()=>{
   })
 
 })
+
+function getToys(){
+  return fetch("http://localhost:3000/toys")
+  .then(resp => resp.json())
+  .then(toys => {
+    toys.forEach(toy => {
+    addToys(toy))
+    })
+  })
+}
+
+function addToys(toy) {
+  const container = document.getElementById('toy-collection')
+  const div = document.createElement('div')
+  div.className = 'card'
+
+  const h2 = document.createElement('h2')
+  h2.innerText = toy.name
+
+  const img = document.createElement('img')
+  img.setAttribute('src', toy.image)
+  img.className = 'toy-avatar')
+
+  const p = document.createElement('p')
+  p.innerText = `${toy.likes} likes`
+
+  let btn = document.createElement('button')
+  btn.className = 'like-btn'
+  btn.innerText = "Like <3"
+
+  div.appendChild(h2, img, p, btn)
+  container.appendChild(div)
+}
