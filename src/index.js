691e2c1a96c23df23c2c9c3e0dfa3029b1c8f108
clonex10100 <clonex10100@gmail.com>
2020-02-21 09:53:38 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -13,7 +13,31 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
     addToys();
+    toyForm.addEventListener('submit', submitToyForm);
 });
+
+function submitToyForm(e) {
+    e.preventDefault()
+    let options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+        },
+        body: JSON.stringify({
+            name: e.target.name.value,
+            image: e.target.image.value,
+            likes: 0
+        })
+    }
+    fetch('http://localhost:3000/toys', options)
+        .then(
+            resp => resp.json()
+        )
+        .then(
+            json => addToyCard(json)
+        );
+}
 
 function addToys(){
     fetch('http://localhost:3000/toys')
@@ -34,15 +58,41 @@ function addToyCard(toy){
 
     let img = document.createElement('img');
     img.src = toy.image;
+    img.class = 'toy-avatar';
     div.appendChild(img);
 
     let p = document.createElement('p');
-    p.textConent = toy.likes;
+    p.textContent = `${toy.likes} Likes`;
     div.appendChild(p);
 
     let button = document.createElement('button');
     button.class = 'like-btn';
+    button.textContent = 'like';
+    button['data-id'] = toy.id;
     div.appendChild(button);
+    button.onclick = like;
 
     document.getElementById('toy-collection').appendChild(div);
+}
+
+function like(e) {
+    let p = e.target.previousSibling;
+    likes = parseInt(p.textContent) + 1;
+    let options = {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+        },
+        body: JSON.stringify({
+            likes: likes
+        })
+    };
+    fetch(`http://localhost:3000/toys/${e.target['data-id']}`, options)
+        .then(
+            resp => resp.json()
+        )
+        .then(
+            json => p.textContent = `${json.likes} Likes`
+        )
 }
